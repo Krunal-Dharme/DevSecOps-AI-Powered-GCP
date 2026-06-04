021b6ae6539a2b04c7b14e49de9e33939475834d
Krunal-Dharme <110104854+Krunal-Dharme@users.noreply.github.com>
2026-06-04 17:59:20 +0530

    Update gke.tf

--- GKE/gke.tf
+++ GKE/gke.tf
@@ -1,7 +1,7 @@
 resource "google_container_cluster" "gke" {
 
   name     = "quantam-gke"
-  location = var.region
+  location = var.zone
 
   deletion_protection = false
 
@@ -16,19 +16,31 @@ resource "google_container_node_pool" "primary_nodes" {
 
   name     = "primary-nodepool"
   cluster  = google_container_cluster.gke.name
-  location = var.region
+  location = var.zone
 
   autoscaling {
     min_node_count = 1
     max_node_count = 2
   }
 
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
   node_config {
 
     machine_type = "e2-medium"
 
+    disk_size_gb = 20
+    disk_type    = "pd-balanced"
+
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
+
+    labels = {
+      environment = "dev"
+    }
   }
 }
