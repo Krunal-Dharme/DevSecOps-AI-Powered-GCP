a4f168c12c15455570a3f115ca4e2362c860e1c9
Krunal-Dharme <110104854+Krunal-Dharme@users.noreply.github.com>
2026-06-06 20:40:27 +0530

    Update trivy-docker-image-scan.sh

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -9,10 +9,18 @@ fi
 echo "Scanning image: $dockerImageName"
 
 # High severity scan
-trivy image --exit-code 0 --severity HIGH $dockerImageName
+trivy image \
+  --severity HIGH,CRITICAL \
+  --format table \
+  -o trivy-report.txt \
+  $dockerImageName
 
-# Critical severity scan (fail if found)
-trivy image --exit-code 1 --severity CRITICAL $dockerImageName
+cat trivy-report.txt
+
+trivy image \
+  --exit-code 1 \
+  --severity CRITICAL \
+  $dockerImageName
 
 exit_code=$?
 echo "Exit Code : $exit_code"
