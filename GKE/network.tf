6ac761ad898a986dff47caea640fda7823005ec7
Krunal-Dharme <110104854+Krunal-Dharme@users.noreply.github.com>
2026-06-10 18:03:29 +0530

    Update network.tf

--- GKE/network.tf
+++ GKE/network.tf
@@ -1,10 +1,10 @@
 resource "google_compute_network" "vpc" {
-  name                    = "quantam-vpc"
+  name                    = "quantam-vpc-new"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "subnet" {
-  name          = "quantam-subnet"
+  name          = "quantam-subnet-new"
   ip_cidr_range = "10.0.1.0/24"
 
   region  = var.region
